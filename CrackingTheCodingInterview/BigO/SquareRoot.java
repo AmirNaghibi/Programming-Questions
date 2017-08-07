d2925deebbdd7228ebc5044a8a4038d131ee7c8d
Amir Naghibi <amirmahdi76@gmail.com>
2017-08-07 11:53:30 -0700

    Added Runtime analysis

--- CrackingTheCodingInterview/BigO/SquareRoot.java
+++ CrackingTheCodingInterview/BigO/SquareRoot.java
@@ -18,3 +18,5 @@ public class SquareRoot{
 		System.out.println(result);
 	}
 }
+/*BigO analysis: the for loop in square function goes from i=0 to i^2=n. In other words, it
+	it goes loops until i=sqrt(n). So O(sqrt(n)) */
