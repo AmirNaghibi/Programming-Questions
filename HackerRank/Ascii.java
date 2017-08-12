a279dd5aa24b172c3c01b4783f721f1ddf5bb9b4
Amir Naghibi <amirmahdi76@gmail.com>
2017-08-11 23:46:39 -0700

    get Ascii code of a char

---
+++ HackerRank/Ascii.java
@@ -0,0 +1,16 @@
+public class Ascii{
+
+	public static void main(String[] args) {
+		String str = "Amir";
+		char[] char_arr = str.toCharArray();
+
+		// for (char c : char_arr) {
+		// 	int index = (int) c;
+		// 	System.out.println(index);	
+		// }	
+		char a = ' ';
+		int index = (int) a;
+		System.out.println(index);
+	}	
+}
+	
