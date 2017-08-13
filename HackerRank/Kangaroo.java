ce74b223165331d4187aea4e7e4d8f2b569d448a
Amir Naghibi <amirmahdi76@gmail.com>
2017-08-12 23:23:08 -0700

    version 1, Not efficient. 30 Tests, 8 failed, 5 timed out

---
+++ HackerRank/Kangaroo.java
@@ -0,0 +1,19 @@
+/*
+	CodeForces Link: https://www.hackerrank.com/challenges/kangaroo
+*/
+import java.util.Scanner;
+
+public class Kangaroo{
+	
+
+	public static void main(String[] args) {
+		Kangaroo obj = new Kangaroo();
+		Scanner scan = new Scanner(System.in);
+
+		int x1 = scan.nextInt();
+		int v1 = scan.nextInt();
+
+		int x2 = scan.nextInt();
+		int v2 = scan.nextInt();
+	}
+}
