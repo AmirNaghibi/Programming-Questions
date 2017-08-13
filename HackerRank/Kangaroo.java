25fbdf4bb0e4e73c83c025399eb6bd2569b1f52a
Amir Naghibi <amirmahdi76@gmail.com>
2017-08-12 23:44:02 -0700

    type casting issue

--- HackerRank/Kangaroo.java
+++ HackerRank/Kangaroo.java
@@ -4,10 +4,29 @@
 import java.util.Scanner;
 
 public class Kangaroo{
+
+	// static String kangaroo(int x1, int v1, int x2, int v2) {
+ //        if(v2>=v1) return "NO";
+ //        // count 
+ //        int c = 1;
+ //        while(x2>x1){
+ //            if(x1+(c*v1) == x2+(c*v2)) return "YES";
+ //            c++;
+ //        }
+ //        return "NO";
+ //    }
+
+	static String kangaroo(int x1, int v1, int x2, int v2) {
+        if(v2>=v1) return "NO";
+        
+        int c = (x2-x1)/(v1-v2);
+
+        if (((x2-x1)/(v1-v2)) == (int)((x2-x1)/(v1-v2)) return "YES";
+        	else return "NO";
+    }
 	
 
 	public static void main(String[] args) {
-		Kangaroo obj = new Kangaroo();
 		Scanner scan = new Scanner(System.in);
 
 		int x1 = scan.nextInt();
@@ -15,5 +34,7 @@ public class Kangaroo{
 
 		int x2 = scan.nextInt();
 		int v2 = scan.nextInt();
+
+		String result = kangaroo(x1,v1,x2,v2);
 	}
 }
