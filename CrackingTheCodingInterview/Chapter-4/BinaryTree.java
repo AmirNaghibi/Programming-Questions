7123b670ca3c497226abbd1e4e4cd00b17703076
Amir Naghibi <amirmahdi76@gmail.com>
2017-09-06 21:37:50 -0700

    modified signature of some methods

--- CrackingTheCodingInterview/Chapter-4/BinaryTree.java
+++ CrackingTheCodingInterview/Chapter-4/BinaryTree.java
@@ -8,11 +8,12 @@
 	- pre_order()
 	- post_order()
 */
-
+import java.util.ArrayList;
+import java.util.AbstractQueue;
 
 public class BinaryTree{
 	
-	private class Node{
+	private static class Node{
 		int data;
 		Node left, right;
 		// constructor
@@ -56,11 +57,38 @@ public class BinaryTree{
 		return currentParent;
 	}
 
+	
+	static boolean isBST(Node node){
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		populateArrayHelper(al,node);
+		return isInAscendingOrder(al);
+	}
+
+	static void populateArrayHelper(ArrayList<Integer> array, Node treeNode){
+		in_order(array, treeNode);
+	}
+
+	static boolean isInAscendingOrder(ArrayList<Integer> array){
+		if (array.size()==0) {
+			System.out.println("Array is Empty");
+			return false;
+		}
+
+		int temp = array.get(0);
+		for(int i=1; i<array.size(); i++){
+			if((int)array.get(i) < temp) return false;
+			else
+				temp = array.get(i);
+		}
+		return true;
+	}
+
+
 
 	//==========================================================
 	// 				   TREE TRAVERSAL METHODS
 	//==========================================================
-	void pre_order(Node root){
+	static void pre_order(Node root){
 		if(root != null){
 			System.out.println(root.data);
 			pre_order(root.left);
@@ -69,16 +97,16 @@ public class BinaryTree{
 	}
 
 
-	void in_order(Node root){
+	static void in_order(ArrayList<Integer> arr,Node root){
 		if(root != null){
-			in_order(root.left);
-			System.out.println(root.data);
-			in_order(root.right);
+			in_order(arr,root.left);
+			arr.add(root.data);
+			in_order(arr,root.right);
 		}
 	}
 
 
-	void post_order(Node root){
+	static void post_order(Node root){
 		if(root != null){
 			post_order(root.left);
 			post_order(root.right);
@@ -87,8 +115,60 @@ public class BinaryTree{
 	}
 
 
+	
+
 
 	public static void main(String[] args) {
+		BinaryTree bTree_search = new BinaryTree();
+
+		Node n1 = new Node(1);
+		Node n2 = new Node(2);
+		Node n3 = new Node(3);
+		Node n4 = new Node(4);
+		Node n5 = new Node(5);
+		Node n6 = new Node(6);
+		Node n7 = new Node(7);
+
+
+		bTree_search.insert(n4);
+		bTree_search.insert(n2);
+		bTree_search.insert(n6);
+		bTree_search.insert(n1);
+		bTree_search.insert(n3);
+		bTree_search.insert(n5);
+		bTree_search.insert(n7);
+
+
+		// n2.left=n5;
+		// n2.right=n1;
+		// n4.left=n6;
+		// n4.right=n3;
+		// n7.left=n2;
+		// n7.right=n4;
+
+		//boolean result2 = bTree_search.isBST(n7);
+
 		
+		// boolean result = bTree_search.isBST(bTree_search.root);
+		// System.out.println(result);
+
+		Node n10 = new Node(10);
+		Node n20 = new Node(20);
+		Node n30 = new Node(30);
+
+		n20.left=n30;
+		n20.right=n10;
+
+		boolean result = isBST(n4);
+		System.out.println(result);
 	}
 }
+
+
+
+
+
+
+
+
+
