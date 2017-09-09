e087e97465259928567c556ea518e3b081295b6b
Amir Naghibi <amirmahdi76@gmail.com>
2017-09-08 19:30:46 -0700

    CamelCase

---
+++ HackerRank/CamelCase.java
@@ -0,0 +1,31 @@
+/*
+	Given a camelCase string with the fist word all lower case, return
+	the total number of words in the string.
+*/
+
+
+
+
+public class CamelCase{
+	
+
+	static int countWords(String s){
+		if(s.length()==0) return 0;
+
+		int total=0;
+
+		for(int i=0; i<s.length(); i++){
+			int ascii = (int) s.charAt(i);
+			if(ascii>= 65 && ascii<=90)
+				total++;
+		}
+		return total+1;
+	}
+
+
+	public static void main(String[] args) {
+		int result = countWords("theUniversityOfBritishColumbia");
+
+		System.out.println(result);
+	}
+}
